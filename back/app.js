c0cba40a8b9770e51e1108cee5e0ceb34320d06a
lbrzr <akakpoleandre0gmail.com>
2024-02-21 14:34:07 +0100

    feat : add cors handling

--- back/app.js
+++ back/app.js
@@ -1,20 +1,23 @@
 require('dotenv').config()
 
 const compression = require('compression')
+const cors = require('cors')
 
 const express = require('express')
 const app = express()
 const port = 3000
 
 const connectDB = require('./config/db');
-connectDB(); 
+connectDB();
 
 const indexRouter = require('./routes/index');
 const booksRouter = require('./routes/books');
 
+app.use(cors());
+app.use(compression());
 app.use('/', indexRouter);
 app.use('/books', booksRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`) 
+  console.log(`Example app listening on port ${port}`)
 })
