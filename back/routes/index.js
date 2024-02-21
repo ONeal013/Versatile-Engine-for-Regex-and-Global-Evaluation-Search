467383dff4d03dceb770b5d16de06c7e0c1dfbbf
Anil <anil013@hotmail.fr>
2024-02-21 11:53:06 +0100

    upadated routes + id endpoints for contents and authors

--- back/routes/index.js
+++ back/routes/index.js
@@ -21,12 +21,27 @@ router.get('/authors', async (req, res) => {
         res.status(500).send({error: error.message});
     }
 });
+
+// Endpoint pour récupérer les auteurs d'un livre spécifique par l'ID du livre
+router.get('/authors/:bookId', async (req, res) => {
+  try {
+      const bookId = req.params.bookId;
+      const book = await Book.findById(bookId).populate('authors');
+      if (book) {
+          res.json(book.authors);
+      } else {
+          res.status(404).send({ message: 'Book not found' });
+      }
+  } catch (error) {
+      res.status(500).send({ error: error.message });
+  }
+});
+
 module.exports = router;
 
 
 
 
-// Endpoint pour récupérer tous les contenus
 router.get('/contents', async (req, res) => {
   try {
       const contents = await Content.find()
@@ -41,6 +56,30 @@ router.get('/contents', async (req, res) => {
       res.status(500).send({error: error.message});
   }
 });
+
+router.get('/contents/:bookId', async (req, res) => {
+  try {
+      const bookId = req.params.bookId;
+      const contents = await Content.find({ book: bookId })
+          .populate({
+              path: 'book',
+              populate: {
+                  path: 'authors' 
+              }
+          });
+      if (contents.length) {
+          res.json(contents);
+      } else {
+          res.status(404).send({ message: 'No content found for this book' });
+      }
+  } catch (error) {
+      res.status(500).send({ error: error.message });
+  }
+});
+
+
+
+
 
 
 
