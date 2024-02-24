37b61e2f56c9b9f4e0076e42468b4d017c080628
Anil <anil013@hotmail.fr>
2024-02-24 23:21:18 +0100

    suggestion for all books added

--- back/routes/suggestions.js
+++ back/routes/suggestions.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const calculateJaccardScore = require('../managers/suggestion');
+const Book = require('../config/models/book');
 
 
 router.get('/', async (req, res) => {
     try {
-        const suggestions = await calculateJaccardScore();
-        res.json(suggestions);
+        const allBooks = await Book.find(); // Récupère tous les livres
+        let allSuggestions = [];
+
+        for (let book of allBooks) {
+            const suggestions = await calculateJaccardScore(book._id.toString());
+            allSuggestions.push({bookId: book._id, suggestions});
+            // Ici, vous pouvez également mettre à jour MongoDB avec les suggestions pour chaque livre
+        }
+
+        res.json(allSuggestions); // Renvoie les suggestions pour tous les livres
     } catch (error) {
         res.status(500).send({ error: error.message });
     }
