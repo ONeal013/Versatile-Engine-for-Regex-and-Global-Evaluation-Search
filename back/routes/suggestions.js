96270dcbd7f7f3dfd7de6138f84ec3cf84334586
Anil <anil013@hotmail.fr>
2024-02-26 14:59:38 +0100

    feat : updated suggestion functionality

--- back/routes/suggestions.js
+++ back/routes/suggestions.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const calculateJaccardScore = require('../managers/suggestion');
 const Book = require('../config/models/book');
 const getPageRankScores = require('../managers/pageRank'); 
+const JaccardScore = require('../config/models/jaccardScore');
+const { ObjectId } = require('mongodb');
 // Assurez-vous du chemin
 
 router.get('/', async (req, res) => {
@@ -39,7 +41,15 @@ router.get('/ranked', async (req, res) => {
 router.get('/:bookId', async (req, res) => {
     try {
         const bookId = req.params.bookId;
-        const suggestions = await calculateJaccardScore(bookId);
+    const suggestions = await JaccardScore.findOne({docId: new ObjectId(bookId)})
+    .populate('docId')
+    .populate({
+        path: 'similarDocs', 
+        populate: { 
+            path: 'docId'
+        }
+    })
+    .exec();
         res.json(suggestions);
     } catch (error) {
         res.status(500).send({ error: error.message });
