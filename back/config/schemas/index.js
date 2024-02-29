02af8b49521ec4de559c52acdb8998db2946d939
Anil <anil013@hotmail.fr>
2024-02-29 12:00:49 +0100

    :init

--- back/config/schemas/index.js
+++ back/config/schemas/index.js
@@ -1,9 +1,9 @@
+// Dans back/config/schemas/index.js
 const mongoose = require('mongoose');
 
 const indexSchema = new mongoose.Schema({
     book: { type: mongoose.Schema.Types.ObjectId, ref: 'Book' },
-    tokens: { type: Map, of: Number }
+    tokens: { type: Object }, // Utiliser un objet pour stocker les tokens
 });
-
 
 module.exports = indexSchema;
--- back/managers/book.js
+++ back/managers/book.js
@@ -4,6 +4,9 @@ const Author = require('../config/models/author');
 const BookContent = require('../config/models/content');
 const Index = require('../config/models/index');
 const tokenize = require('./indexor');
+const fs = require('fs');
+const path = require('path'); 
+
 
 function cleanKeys(tokens) {
   const cleanedTokens = {};
@@ -16,72 +19,99 @@ function cleanKeys(tokens) {
 }
 
 
-async function fetchAndStoreBooks() {
+async function fetchAndStoreBooks(totalBooks = 1000) {
     try {
-        // Récupérer les 10 premiers livres de Gutenberg
-        const response = await axios.get('https://gutendex.com/books/');
-        const books = response.data.results;
+        const booksPerPage = 100; // Assurez-vous que ce nombre est correct selon l'API
+        const totalPages = Math.ceil(totalBooks / booksPerPage); // Calculer le nombre total de pages nécessaires
 
-        for (const book of books) {
-            // Gérer les auteurs
-            const authors = await Promise.all(book.authors.map(async (authorData) => {
-                let author = await Author.findOne({ name: authorData.name });
-                if (!author) {
-                    author = new Author(authorData);
-                    await author.save();
-                }
-                return author;
-            }));
-            // Gérer les traducteurs
-            const translators = await Promise.all(book.translators.map(async (translatorData) => {
-                let translator = await Author.findOne({ name: translatorData.name });
-                if (!translator) {
-                    translator = new Author(translatorData);
-                    await translator.save();
-                }
-                return translator;
-            }));
+        for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+            const response = await axios.get(`https://gutendex.com/books/?page=${currentPage}`);
+            const books = response.data.results;
 
-            // Créer et sauvegarder le nouveau livre
-            const newBook = new Book({
-                ...book,
-                authors,
-                translators,
-            });
-            await newBook.save();
-            console.log(`Livre "${newBook.title}" ajouté à la base de données.`);
+            for (const book of books) {
+                const authors = await Promise.all(book.authors.map(async (authorData) => {
+                    let author = await Author.findOne({ name: authorData.name });
+                    if (!author) {
+                        author = new Author(authorData);
+                        await author.save();
+                    }
+                    return author;
+                }));
 
-            // Vérifier si le format de contenu textuel est disponible pour le livre
-            const text = newBook.formats['text/plain; charset=us-ascii'] ?? newBook.formats['text/plain; charset=utf-8']
-            if (text) {
-                const contentResponse = await axios.get(text);
-                const content = contentResponse.data;
+                const translators = await Promise.all(book.translators.map(async (translatorData) => {
+                    let translator = await Author.findOne({ name: translatorData.name });
+                    if (!translator) {
+                        translator = new Author(translatorData);
+                        await translator.save();
+                    }
+                    return translator;
+                }));
 
-                // Sauvegarder le contenu du livre
-                const newBookContent = new BookContent({
-                    book: newBook._id,
-                    content: content,
+                const newBook = new Book({
+                    ...book,
+                    authors,
+                    translators,
                 });
-                await newBookContent.save();
+                await newBook.save();
+                console.log(`Livre "${newBook.title}" ajouté à la base de données.`);
 
-                // Tokeniser et indexer le contenu du livre
-                const tokens = tokenize(content); // Supposons que c'est votre objet original
-                const cleanedTokens = cleanKeys(tokens);
-                const newIndex = new Index({
-                    book: newBook._id,
-                    tokens: cleanedTokens,
-                });
-                await newIndex.save();
-            } else {
-                console.log(`Format de contenu texte non trouvé pour le livre "${newBook.title}".`);
+                const text = newBook.formats['text/plain; charset=us-ascii'] ?? newBook.formats['text/plain; charset=utf-8'];
+                if (text) {
+                    const tempFilePath = path.join(__dirname, '..', 'temp', `${newBook._id}.txt`);
+                    fs.mkdirSync(path.dirname(tempFilePath), { recursive: true });
+
+                    const writer = fs.createWriteStream(tempFilePath);
+
+                    try {
+                        const response = await axios({
+                            method: 'get',
+                            url: text,
+                            responseType: 'stream'
+                        });
+
+                        response.data.pipe(writer);
+
+                        await new Promise((resolve, reject) => {
+                            writer.on('finish', resolve);
+                            writer.on('error', reject);
+                        });
+
+                        const content = fs.readFileSync(tempFilePath, 'utf-8');
+
+                        if (Buffer.byteLength(content, 'utf-8') > (16 * 1024 * 1024)) {
+                            console.log(`Le livre "${newBook.title}" est trop volumineux et sera ignoré.`);
+                            continue;
+                        }
+
+                        const newBookContent = new BookContent({
+                            book: newBook._id,
+                            content: content,
+                        });
+                        await newBookContent.save();
+
+                        const tokens = tokenize(content);
+                        const cleanedTokens = cleanKeys(tokens);
+
+                        const newIndex = new Index({
+                            book: newBook._id,
+                            tokens: cleanedTokens,
+                        });
+                        await newIndex.save();
+                    } catch (error) {
+                        console.error(`Erreur lors du téléchargement du contenu pour le livre "${newBook.title}":`, error);
+                    } finally {
+                        fs.unlinkSync(tempFilePath);
+                    }
+                } else {
+                    console.log(`Format de contenu texte non trouvé pour le livre "${newBook.title}".`);
+                }
             }
         }
-        console.log('Opération terminée.');
+        console.log('Opération terminée avec succès pour 1000 livres.');
     } catch (error) {
         console.error('Erreur lors de la récupération et du stockage des livres:', error);
     }
 }
-
 
 async function reverseIndex() {
     const indexes = await Index.find();
